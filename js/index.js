17cacdeb3bab3f93d68585c708de9ff01e248851
Ronaldo Meza <ronaldo.meza@tecsup.edu.pe>
2025-08-03 16:13:40 -0500

    Front DigiRest 15%

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
+const wrapper = document.getElementById('wrapper');
+const btnToggle = document.getElementById('btn-toggle');
 
-
+btnToggle.addEventListener('click', () => {
+  const isOpen = wrapper.classList.toggle('open');
+  btnToggle.textContent = isOpen
+    ? 'CANCELAR RESERVA'
+    : 'RESERVAR EN LINEA ';
+});
